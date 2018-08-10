1015e8af1a32d62854f1e8a448ef6897730bd02b
unknown <alfarobr@gmail.com>
2018-08-10 00:38:43 -0500

    Avance 10/08/2018 12:37am

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
+	AOS.init();
 	$('.categories .item').matchHeight();
 	$("#slider-customers").lightSlider({
 		item: 5,
 		speed:600,
 		auto: true
-	}); 
+	});
+	$('#carousel-home').carousel({
+		interval: 8000
+	});	  
 });
