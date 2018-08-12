67e7afe59f2179a3bf5de7ff4a6b5f46bf01665e
unknown <alfarobr@gmail.com>
2018-08-12 03:13:26 -0500

    Avance 12/08/2018 3:12am

--- js/main.js
+++ js/main.js
@@ -1,73 +1,85 @@
 $(document).ready(function(){
-	AOS.init();
+	//AOS.init();
 	$('#carousel-home').carousel({
 		interval: 8000
 	});
-	$('.categories .item').matchHeight();
+
+	$('.categories .item, .page-category .item').matchHeight();
+
 	$("#slider-customers").lightSlider({
 		item: 5,
-		speed:600,
+		speed: 800,
 		auto: true,
-		loop: true
+		loop: true,
+		pauseOnHover: true,
+		responsive : [
+			{
+				breakpoint:992,
+				settings: {
+					item:3,
+					slideMove:1,
+					slideMargin:6,
+				  }
+			},
+			{
+				breakpoint:768,
+				settings: {
+					item:2,
+					slideMove:1
+				  }
+			},
+			{
+				breakpoint:576,
+				settings: {
+					item:1,
+					slideMove:1
+				  }
+			}
+		]
 	});
 
-	$('#map').vectorMap({
-		//map: 'world_mill',
-		normalizeFunction: 'polynomial',
-		hoverOpacity     : 0.7,
-		hoverColor       : false,
-		backgroundColor  : 'transparent',
-		regionStyle      : {
-		initial      : {
-			fill            : 'rgba(210, 214, 222, 1)',
-			'fill-opacity'  : 1,
-			stroke          : 'none',
-			'stroke-width'  : 0,
-			'stroke-opacity': 1
-		},
-		hover        : {
-			'fill-opacity': 0.7,
-			cursor        : 'pointer'
-		},
-		selected     : {
-			fill: 'yellow'
-		},
-		selectedHover: {}
-		},
-		markerStyle      : {
-		initial: {
-			fill  : '#d40000',
-			stroke: '#fff'
-		}
-		},
-		markers          : [
-		{ latLng: [41.90, 12.45], name: 'Vatican City' },
-		{ latLng: [43.73, 7.41], name: 'Monaco' },
-		{ latLng: [-0.52, 166.93], name: 'Nauru' },
-		{ latLng: [-8.51, 179.21], name: 'Tuvalu' },
-		{ latLng: [43.93, 12.46], name: 'San Marino' },
-		{ latLng: [47.14, 9.52], name: 'Liechtenstein' },
-		{ latLng: [7.11, 171.06], name: 'Marshall Islands' },
-		{ latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis' },
-		{ latLng: [3.2, 73.22], name: 'Maldives' },
-		{ latLng: [35.88, 14.5], name: 'Malta' },
-		{ latLng: [12.05, -61.75], name: 'Grenada' },
-		{ latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines' },
-		{ latLng: [13.16, -59.55], name: 'Barbados' },
-		{ latLng: [17.11, -61.85], name: 'Antigua and Barbuda' },
-		{ latLng: [-4.61, 55.45], name: 'Seychelles' },
-		{ latLng: [7.35, 134.46], name: 'Palau' },
-		{ latLng: [42.5, 1.51], name: 'Andorra' },
-		{ latLng: [14.01, -60.98], name: 'Saint Lucia' },
-		{ latLng: [6.91, 158.18], name: 'Federated States of Micronesia' },
-		{ latLng: [1.3, 103.8], name: 'Singapore' },
-		{ latLng: [1.46, 173.03], name: 'Kiribati' },
-		{ latLng: [-21.13, -175.2], name: 'Tonga' },
-		{ latLng: [15.3, -61.38], name: 'Dominica' },
-		{ latLng: [-20.2, 57.5], name: 'Mauritius' },
-		{ latLng: [26.02, 50.55], name: 'Bahrain' },
-		{ latLng: [0.33, 6.73], name: 'São Tomé and Príncipe' },
-		{ latLng: [-33.43,-70.65], name: 'DQS en Chile | Av. Santa María 0214 - Santiago | Contacto: dqsacademy@dqs-chile.com'}
+	$("#slider-business-related").lightSlider({
+		item: 4,
+		speed: 800,
+		auto: true,
+		loop: true,
+		dots: false,
+		pauseOnHover: true,
+		pager: true,
+		responsive : [
+			{
+				breakpoint:992,
+				settings: {
+					item:3,
+					slideMove:1,
+					slideMargin:6,
+				  }
+			},
+			{
+				breakpoint:768,
+				settings: {
+					item:2,
+					slideMove:1
+				  }
+			},
+			{
+				breakpoint:576,
+				settings: {
+					item:1,
+					slideMove:1
+				  }
+			}
 		]
-	  });
+	});
+
+	$("#slider-logo-course").lightSlider({
+		item: 1,
+		speed: 800,
+		auto: true,
+		loop: true,
+		dots: false,
+		pauseOnHover: true,
+		pager: true,
+	});
+
 });
